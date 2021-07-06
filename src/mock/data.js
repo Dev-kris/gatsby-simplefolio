fc78aabf6ab918089c0b9d2009347a2b52513222
Kris Long <corpkris@gmail.com>
2021-07-06 17:59:48 +0200

    updated content and profile picture

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Kris | Web Devloper', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcom to my web development portfolio.', // e.g: Welcome to my website
 };
 
 // HERO DATA
@@ -17,7 +17,7 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpeg',
+  img: 'profile.png',
   paragraphOne:
     'I was the boy who would take everything apart to find out how it works, or to make something new.Whether is was giving life to a NEC laptop or finding out how to remove the governor on a gokart, I was always driven by this need to explore and create.',
   paragraphTwo:
@@ -36,7 +36,7 @@ export const projectsData = [
     info: 'This is a fullstack project leveraging Next.js, Express, TypeORM, TypeScript, and MySQL',
     info2:
       'A company wanted to host a similar to Reddit to encourage casual communication and improve connectedness. I built this app to be used internally on their servers, using their existing SQL infrastructure.',
-    url: '',
+    url: 'https://github.com/Dev-kris/social_app',
     repo: 'https://github.com/Dev-kris/social_app', // if no repo, the button will not show up
   },
   {
@@ -46,7 +46,7 @@ export const projectsData = [
     info: 'RESTful API using Express, MongoDB Atlas, and Mongoose.',
     info2:
       'With a frontend already developed, I was made to create a secure API and database to bring this hot sauce review application to life.',
-    url: '',
+    url: 'https://github.com/Dev-kris/Pekocko',
     repo: 'https://github.com/Dev-kris/Pekocko', // if no repo, the button will not show up
   },
   {
@@ -56,14 +56,14 @@ export const projectsData = [
     info: 'eCommerce website built using only HTML, CSS, Bootstrap 5, and Vanilla JS',
     info2:
       'This web store needed to be built using only vanilla JS and basic css libraries. It was then connected to an existing API and database to retrieve product data dynamically.',
-    url: '',
+    url: 'https://github.com/Dev-kris/Vintera',
     repo: 'https://github.com/Dev-kris/Vintera', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: 'click',
+  cta: 'Want to chat? Send me an email!',
   btn: '',
   email: 'kristopher.j.long@gmail.com',
 };
