f9e07b436396dbfe2137adee3eee7809067eeab0
Kris Long <corpkris@gmail.com>
2021-07-12 15:58:59 +0200

    modified timing and entrance fade

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -16,58 +16,54 @@ const Contact = () => {
   const { isEnabled } = githubButtons;
 
   return (
-    <>
-      <Fade bottom duration={1000} delay={800} distance="200px">
-        <section id="contact">
-          <Container>
-            <Title title="Contact" />
-            <Fade right duration={11000} delay={800} distance="30px">
-              <div className="contact-wrapper">
-                <p className="contact-wrapper__text">
-                  {cta || 'Would you like to work with me? Awesome!'}
-                </p>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="cta-btn cta-btn--resume"
-                  href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
-                >
-                  {btn || "Let's Talk"}
-                </a>
-              </div>
-            </Fade>
-          </Container>
-          <br />
-          <footer className="pt-5">
-            <Container>
-              <span className="back-to-top"></span>
-              <div className="social-links">
-                {networks &&
-                  networks.map((network) => {
-                    const { id, name, url } = network;
-                    return (
-                      <a
-                        key={id}
-                        href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                        aria-label={name}
-                      >
-                        <i className={`fa fa-${name || 'refresh'} fa-inverse`} />
-                      </a>
-                    );
-                  })}
-              </div>
+    <section id="contact">
+      <Container>
+        <Fade right duration={1000} delay={800} distance="300px">
+          <Title title="Contact" />
+          <div className="contact-wrapper">
+            <p className="contact-wrapper__text">
+              {cta || 'Would you like to work with me? Awesome!'}
+            </p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cta-btn cta-btn--resume"
+              href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
+            >
+              {btn || "Let's Talk"}
+            </a>
+          </div>
+        </Fade>
+      </Container>
+      <br />
+      <footer className="pt-5">
+        <Container>
+          <span className="back-to-top"></span>
+          <div className="social-links">
+            {networks &&
+              networks.map((network) => {
+                const { id, name, url } = network;
+                return (
+                  <a
+                    key={id}
+                    href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    aria-label={name}
+                  >
+                    <i className={`fa fa-${name || 'refresh'} fa-inverse`} />
+                  </a>
+                );
+              })}
+          </div>
 
-              {isEnabled && <GithubButtons />}
-              <Link to="hero" smooth duration={1000}>
-                <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
-              </Link>
-            </Container>
-          </footer>
-        </section>
-      </Fade>
-    </>
+          {isEnabled && <GithubButtons />}
+          <Link to="hero" smooth duration={1000}>
+            <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
+          </Link>
+        </Container>
+      </footer>
+    </section>
   );
 };
 
