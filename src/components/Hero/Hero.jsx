74863b1eb9b0d85c17bf7f50885c890312e08d61
Kris Long <corpkris@gmail.com>
2021-07-12 15:59:12 +0200

    hardcoded name and subtitle data

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3542,13 +3542,13 @@ const Header = () => {
             <span>e</span> {''}
             <span>i</span>
             <span>s</span> {''}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-main">Kristopher Long</span>
             <br />
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {'Web Developer'}
           </h1>
         </Fade>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
